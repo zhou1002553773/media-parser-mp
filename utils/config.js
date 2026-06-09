9cad1d4ff5d8e37a996f928b8b699f6a85c6cec8
zyh <1002553773@qq.com>
2026-06-09 11:17:12 +0800

    config

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@
 const config = {
   // 后端服务器基础域名 (部署时请替换为您自己的域名)
   // 注意：必须以 https:// 开头，且末尾不要带斜杠 /
-  baseURL: 'http://39.105.85.1:8051',
+  baseURL: 'https://parse.aiideas.top',
   // baseURL: 'http://127.0.0.1:8051',
   
   // 请求超时时间（毫秒）
